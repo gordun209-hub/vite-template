bf4b1bc74378cef7d566fa4ac0e1f2ecec4d2f74
gordun209 <ali-han80@hotmail.com>
2022-02-24 13:05:51 +0300

    added jest rtl and postcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+
+import { useState } from 'react'
+
+import logo from './logo.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
+
         <p>Hello Vite + React!</p>
         <p>
           <button type='button' onClick={() => setCount((count) => count + 1)}>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
+import legacy from '@vitejs/plugin-legacy'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactRefresh(), legacy()],
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
