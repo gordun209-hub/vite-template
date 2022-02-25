f7dcbc44179e522d6172e4de57338eef0779bb88
gordun209 <ali-han80@hotmail.com>
2022-02-25 19:47:43 +0300

    configured prettier && added redux and styled components also some configure about vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
 import legacy from '@vitejs/plugin-legacy'
+import react from '@vitejs/plugin-react'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), reactRefresh(), legacy()],
   esbuild: {
-    jsxInject: `import React from 'react'`,
+    jsxInject: `import React from 'react'`
   },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        secure: false,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
